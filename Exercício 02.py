3c88aab8352d67d104e6132edf26ff6f4d696830
Bruno Cassilhas <86086324+bcassilhas@users.noreply.github.com>
2022-07-12 23:56:02 -0300

    Add files via upload

---
+++ Exercício 02.py
@@ -0,0 +1,10 @@
+## Exercício 03 - Seção 4
+
+
+num1 = int(input('Digite o primeiro número inteiro:'))
+num2 = int(input('Digite o segundo número inteiro:'))
+num3 = int(input('Digite o terceiro número inteiro:'))
+
+soma = num1 + num2 + num3
+
+print(f'A soma dos três números é: {soma}.')
